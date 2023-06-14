36a8273f05789f6c4dfc6d0d57c1586f676a0775
PainAiko <55332444+PainAiko@users.noreply.github.com>
2023-06-14 22:02:49 +0300

    2 files modified

--- demo.tsx
+++ demo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.css';
 import { Button, QRCode } from 'antd';
 import { useReactToPrint } from 'react-to-print';
-
+import JsPDF from 'jspdf';
 const downloadQRCode = () => {
   const canvas: any = document.getElementById('myqrcode');
   // ?.querySelector<HTMLCanvasElement>('canvas');
@@ -24,12 +24,35 @@ const App: React.FC = () => {
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
   });
+  const generatePDF = () => {
+    const doc = new JsPDF('portrait', 'pt', 'a4');
+    console.log(doc);
+    doc.html(document.querySelector('#report') as HTMLElement).then(() => {
+      doc.save('report.pdf');
+    });
+  };
+
+  const downloadQRCode = () => {
+    const canvas = document
+      .getElementById('myqrcode')
+      ?.querySelector<HTMLCanvasElement>('canvas');
+    if (canvas) {
+      const url = canvas.toDataURL();
+      const a = document.createElement('a');
+      a.download = 'QRCode.png';
+      a.href = url;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+  };
+
   return (
     <div id="myqrcode">
-      <div ref={componentRef}>
-      <QRCode  value="https://ant.design/" style={{ marginBottom: 16 }} />
+      <div id="report" ref={componentRef}>
+        <QRCode value="https://ant.design/" style={{ marginBottom: 16 }} />
       </div>
-      <Button type="primary" onClick={handlePrint}>
+      <Button type="primary" onClick={downloadQRCode}>
         Download
       </Button>
     </div>
